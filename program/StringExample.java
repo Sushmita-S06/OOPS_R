16f36981f7bdae92de28484973cf32d432051017
Sushmita-S06 <sushmitasallam@gmail.com>
2025-07-29 11:03:48 +0530

    Add files via upload

---
+++ program/StringExample.java
@@ -0,0 +1,13 @@
+package program;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String greeting = "Hello, World!";
+        System.out.println(greeting);
+
+
+	}
+
+}
